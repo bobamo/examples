f51470b4ae13739f038d42fda413035fa01f3a53
jspears <speajus@gmail.com>
2012-08-27 16:38:35 -0400

    *small fix to allow plugins to function correctly.

--- simple/app.js
+++ simple/app.js
@@ -8,7 +8,6 @@ var express = require('express')
     , Group = require('./model/group')
     , Employee = require('./model/employee')
     , bobamo = require('bobamo')
-    , http = require('http')
     ;
 
 var app = module.exports = express();
@@ -16,33 +15,18 @@ var app = module.exports = express();
 // Configuration
 
 app.configure(function () {
-//    app.set('views', __dirname + '/views');
-//    app.set('view engine', 'jade');
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
     app.use(express.session({secret:'super duper secret'}))
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function () {
-    app.use('/bobamo', bobamo.express({plugin:['session','imageupload','visualize'], uri:'mongodb://localhost/bobamo_development'}));
+    app.use(bobamo.bobamo({uri:'mongodb://localhost/bobamo_development'}));
 
     app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
+
 });
-
-app.configure('production', function () {
-    app.use('/bobamo', bobamo.express({uri:'mongodb://localhost/bobamo'}, express));
-
-    app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', function(req,res){ res.render('redir_index.html', {layout:false})});
 
 app.listen(3001);
-http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'), app.settings.env);
-});
+
+console.log('Express server listening on port 3001');
